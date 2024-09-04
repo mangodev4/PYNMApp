a7ed2e3edafb3a9487308d41bb64c8729c8ce6b9
Mango <mangodev4@gmail.com>
2024-09-04 17:30:43 +0900

    feat. Haptic Manager 추가

---
+++ PYNMApp/Managers/HapticManager.swift
@@ -0,0 +1,18 @@
+//
+//  HapticManager.swift
+//  PYNMApp
+//
+//  Created by Yujin Son on 9/4/24.
+//
+
+import SwiftUI
+
+struct HapticManager: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HapticManager()
+}
