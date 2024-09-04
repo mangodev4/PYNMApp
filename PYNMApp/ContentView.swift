0b701529e6e471fd546f7958f0880c7f079da04b
Mango <mangodev4@gmail.com>
2024-09-04 14:32:59 +0900

    Initial Commit

---
+++ PYNMApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PYNMApp
+//
+//  Created by Yujin Son on 9/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PYNMApp/PYNMAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PYNMAppApp.swift
+//  PYNMApp
+//
+//  Created by Yujin Son on 9/4/24.
+//
+
+import SwiftUI
+
+@main
+struct PYNMAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
