0d73dcd2d902000a9cb999dfb2d26738cb69edd5
Mango <mangodev4@gmail.com>
2024-09-05 01:14:29 +0900

    feat. 메인뷰 카드리스트 사이즈 & 간격 조정
    카드가 리스트에 꽉 차게 frame 및 padding 값 조정

--- PYNMApp/Views/MainView.swift
+++ PYNMApp/Views/MainView.swift
@@ -14,14 +14,21 @@ struct MainView: View {
         NavigationView {
             List(viewModel.cards) { card in
                 NavigationLink(destination: CardDetailView(viewModel: viewModel, card: card)) {
-                    Image(card.imageName)
-                        .resizable()
-                        .scaledToFit()
-                        .frame(height: 100)
-                        .opacity(card.opacity)
-                        .padding()
+                    ZStack {
+                        Image(card.imageName)
+                            .resizable()
+                            .scaledToFill()
+                            .frame(height: 140)
+                            .opacity(card.opacity)
+//                            .padding()
+                    }
+                    .frame(maxWidth: .infinity)
+                    .padding(.vertical, 2)
                 }
+                .buttonStyle(PlainButtonStyle())
             }
+            .listStyle(PlainListStyle())
+            .listRowSeparator(.hidden)
             .navigationTitle("평양냉면")
         }
     }
