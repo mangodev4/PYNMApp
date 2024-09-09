8e4864bc569faed3c2ca3bddb92300bea94d1fa0
Mango <mangodev4@gmail.com>
2024-09-09 15:04:07 +0900

    feat. 메인뷰 상단뷰 StickyHeader 구현
    pinnedViews로 sectionHeader 고정

--- PYNMApp/Views/MainView.swift
+++ PYNMApp/Views/MainView.swift
@@ -14,46 +14,78 @@ struct MainView: View {
     var body: some View {
         NavigationStack(path: $navigationManager.path) {
             ScrollView {
-                LazyVStack(spacing: 10) {
-                    ForEach(viewModel.cards) { card in
-                        ZStack {
-                            Button(action: {
-                                navigationManager.navigateToCardDetail(card: card)
-                            }) {
-                                Image(card.imageName)
-                                    .resizable()
-                                    .scaledToFill()
-                                    .cornerRadius(8)
-                                    .opacity(card.opacity)
+                ZStack {
+//                    Image(systemName: "photo.fill")
+//                        .font(.title)
+//                        .foregroundColor(.brown)
+                    Image("PYNM_Header")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill )
+                        .clipped()
+                    Text("평양냉면 헤더 예시입니다")
+                        .font(.title)
+                        .foregroundStyle(.white)
+                }
+                LazyVStack(pinnedViews:[.sectionHeaders]) {
+                    Section(header: Header()) {
+                        ForEach(viewModel.cards) { card in
+                            ZStack {
+                                Button(action: {
+                                    navigationManager.navigateToCardDetail(card: card)
+                                }) {
+                                    Image(card.imageName)
+                                        .resizable()
+                                        .scaledToFill()
+                                        .cornerRadius(8)
+                                        .opacity(card.opacity)
+                                }
                             }
+                            .frame(maxWidth: .infinity)
+                            .padding(.vertical, 3)
+                            .padding(.horizontal, 5)
+                            
                         }
-                        .frame(maxWidth: .infinity)
-                        .padding(.vertical, 3)
-                        .padding(.horizontal, 5)
-                        
                     }
                 }
             }
-            .navigationTitle("평양냉면")
-            .navigationBarTitleDisplayMode(.inline)
-            .toolbar {
-                ToolbarItem(placement: .principal) {
-                    VStack {
-                        Text("평양냉면 뿌시기")
-                            .font(.headline)
-                        //                    Image(systemName: "photo.fill")
-                        //                        .font(.title)
-                        //                        .foregroundColor(.brown)
-                        Text("뿌셔뿌셔")
-                            .font(.subheadline)
-                    }
-                }
-            }
+            .clipped()
+//            .navigationTitle("평양냉면")
+//            .navigationBarTitleDisplayMode(.inline)
+//            .toolbar {
+//                ToolbarItem(placement: .principal) {
+//                    VStack {
+//                        Text("평양냉면 뿌시기")
+//                            .font(.headline)
+//                        Image(systemName: "photo.fill")
+//                            .font(.title)
+//                            .foregroundColor(.brown)
+//                            .padding()
+//                        Text("뿌셔뿌셔")
+//                            .font(.subheadline)
+//                    }
+//                }
+//            }
             .navigationDestination(for: Card.self) { card in
                 CardDetailView(viewModel: viewModel, card: card)
                     .environmentObject(navigationManager)
             }
         }
         .environmentObject(navigationManager)
+    }
+}
+
+struct Header: View {
+    var body: some View {
+        VStack {
+            Spacer()
+            Text("평양 냉면")
+                .fontWeight(.bold)
+            Text("서울 맛집 20선")
+            Spacer()
+            Divider()
+        }
+        .frame(minWidth: 0, maxWidth: .infinity)
+        .frame(height: 50)
+        .background(Rectangle().foregroundColor(.white))
     }
 }
