8592d7247853aebc9e8c195476e978a262dbe2b0
Mango <mangodev4@gmail.com>
2024-09-11 18:07:20 +0900

    feat. 맵뷰 어노테이션 스위치 버튼 구현
    임시로 버튼 탭할 시 맵 센터값이 지정한 place로 전환되게 구현

--- PYNMApp/Models/MapViewModel.swift
+++ PYNMApp/Models/MapViewModel.swift
@@ -15,8 +15,8 @@ class MapViewModel: ObservableObject {
         // 샘플 데이터 추가
         places = [
             Place(name: "Garage", coordinate: CLLocationCoordinate2D(latitude: 37.503774633134, longitude: 127.048192060882)),
-            Place(name: "Place 1", coordinate: CLLocationCoordinate2D(latitude: 40.83757722488077, longitude: 14.306896671048852)),
-            Place(name: "Place 2", coordinate: CLLocationCoordinate2D(latitude: 40.83857722488077, longitude: 14.306896671048852)),
+            Place(name: "Place 1", coordinate: CLLocationCoordinate2D(latitude: 37.5032022121152, longitude: 127.049313250519)),
+            Place(name: "Place 2", coordinate: CLLocationCoordinate2D(latitude: 37.5035470555751, longitude: 127.047033596011)),
             // 나머지 17개 장소 추가
         ]
     }
--- PYNMApp/Views/MapView.swift
+++ PYNMApp/Views/MapView.swift
@@ -11,37 +11,56 @@ import MapKit
 struct MapView: View {
     
     @StateObject private var viewModel = MapViewModel()
+    @State private var region: MKCoordinateRegion
+    @State private var currentIndex = 0
     
-//    let garage = CLLocationCoordinate2D(latitude: 40.83657722488077, longitude: 14.306896671048852)
+    init() {
+        _region = State(initialValue: MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: 37.503774633134, longitude: 127.048192060882),
+            span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+        ))
+    }
     
-        var body: some View {
-            Map(coordinateRegion: .constant(MKCoordinateRegion(
-                       center: CLLocationCoordinate2D(latitude: 37.503774633134, longitude: 127.048192060882),
-                       span: MKCoordinateSpan(latitudeDelta: 0.003, longitudeDelta: 0.003)
-                   )), annotationItems: viewModel.places) { place in
-                       MapAnnotation(coordinate: place.coordinate) {
-                           ZStack {
-                               RoundedRectangle(cornerRadius: 5)
-                                   .fill(Color.white.opacity(0.8))
-                               RoundedRectangle(cornerRadius: 5)
-                                   .stroke(Color.secondary, lineWidth: 2)
-                               VStack {
-                                   Image(systemName: "building.fill")
-                                       .foregroundColor(.blue)
-                                       .padding(5)
-                                   Text(place.name)
-                                       .font(.caption)
-                                       .padding(5)
-                               }
-                           }
-                       }
-                   }
-                   .onAppear {
-                       viewModel.loadPlaces()
-                   }
-                   .edgesIgnoringSafeArea(.all)
-               }
-           }
+    
+    var body: some View {
+        ZStack {
+            Map(coordinateRegion: $region, annotationItems: viewModel.places) { place in
+                MapAnnotation(coordinate: place.coordinate) {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 5)
+                            .fill(Color.white.opacity(0.8))
+                        RoundedRectangle(cornerRadius: 5)
+                            .stroke(Color.secondary, lineWidth: 2)
+                        VStack {
+                            Image(systemName: "building.fill")
+                                .foregroundColor(.blue)
+                                .padding(5)
+                            Text(place.name)
+                                .font(.caption)
+                                .padding(5)
+                        }
+                        
+                    }
+                }
+            }
+            .onAppear {
+                viewModel.loadPlaces()
+            }
+            .edgesIgnoringSafeArea(.all)
+        }
+        
+        Button {
+            withAnimation {
+                currentIndex = (currentIndex + 1) % viewModel.places.count
+                region.center = viewModel.places[currentIndex].coordinate
+                region.span = MKCoordinateSpan(latitudeDelta: 0.005, longitudeDelta: 0.005)
+            }
+        } label: {
+            Text("Teleport")
+        }
+        .buttonStyle(OnboardingButtonStyle())
+    }
+}
 
 #Preview {
     MapView()
