58aa1f668bd8e37d0733f194ea88cfd04eeae2d9
Mango <mangodev4@gmail.com>
2024-09-11 14:06:25 +0900

    feat. 맵뷰 생성 구현
    메인뷰 기획 변경 및 지도 연동을 위한 맵뷰 생성

---
+++ PYNMApp/Views/MapView.swift
@@ -0,0 +1,19 @@
+//
+//  MapView.swift
+//  PYNMApp
+//
+//  Created by Yujin Son on 9/11/24.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MapView()
+}
