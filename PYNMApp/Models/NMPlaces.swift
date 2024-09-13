c222ca2dd75c82db79b4b7473c927fec3f34c4a2
Mango <mangodev4@gmail.com>
2024-09-13 13:45:39 +0900

    feat. NMPlaces 모델 생성 및 기존 모델 제거 구현
    NM 플레이스, 좌표를 따로 관리하기 위해 모델 구분 생성

---
+++ PYNMApp/Models/NMPlaces.swift
@@ -0,0 +1,24 @@
+//
+//  Places.swift
+//  PYNMApp
+//
+//  Created by Yujin Son on 9/13/24.
+//
+
+import Foundation
+import MapKit
+
+struct NMPlace: Identifiable {
+    let id = UUID()
+    let name: String
+    let coordinate: CLLocationCoordinate2D
+}
+
+let sampleNMPlaces: [NMPlace] = [
+    NMPlace(name: "Garage", coordinate: CLLocationCoordinate2D(latitude: 37.503774633134, longitude: 127.048192060882)),
+    NMPlace(name: "Place 1", coordinate: CLLocationCoordinate2D(latitude: 37.5085953316928, longitude: 127.059718502493)),
+    NMPlace(name: "Place 2", coordinate: CLLocationCoordinate2D(latitude: 37.5073677154051, longitude: 127.047165945717)),
+    NMPlace(name: "Place 3", coordinate: CLLocationCoordinate2D(latitude: 37.5032022121152, longitude: 127.049313250519)),
+    NMPlace(name: "Place 4", coordinate: CLLocationCoordinate2D(latitude: 37.5035470555751, longitude: 127.047033596011))
+    // 나머지 장소 추가
+]
