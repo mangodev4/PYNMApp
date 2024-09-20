3052f5285c3f553497ee861f06776e010abeb8c7
Mango <mangodev4@gmail.com>
2024-09-20 17:16:44 +0900

    feat. 바텀시트뷰 파일 교체 구현
    기존 바텀시트뷰 제거 후 새 바텀시트뷰로 대체

--- PYNMApp/Views/CarouselView.swift
+++ PYNMApp/Views/CarouselView.swift
@@ -1,76 +1,55 @@
-////
-////  CarouselView.swift
-////  PYNMApp
-////
-////  Created by Yujin Son on 9/20/24.
-////
 //
-//import SwiftUI
+//  SheetTestView.swift
+//  PYNMApp
 //
-//struct CarouselView: View {
-//    @ObservedObject var mapViewModel = MapViewModel()
-//    @ObservedObject var cardListViewModel = CardListViewModel()
+//  Created by Yujin Son on 9/20/24.
 //
-//    var body: some View {
-//        VStack {
+
+import SwiftUI
+
+struct SheetTestView: View {
+    @ObservedObject var viewModel = CardListViewModel()
+    @ObservedObject var mapViewModel = MapViewModel()
+    @State private var selectedIndex = 0
+
+    
+    var body: some View {
+        VStack {
 //            Spacer()
 //            
 //            Capsule()
 //                .frame(width: 40, height: 6)
 //                .foregroundColor(.gray)
 //                .padding(.top, 8)
-//            
-//            TabView {
-//                ForEach(cardListViewModel.cards) { card in
-//                    VStack(spacing: 15) {
-//                        Image(card.imageName)
-//                            .resizable()
-//                            .scaledToFit()
-//                            .frame(width: 100, height: 100)
-//                        
-//                        Text(card.imageName)
-//                            .font(.headline)
-//                        
-//                        Text(card.description)
-//                            .font(.subheadline)
-//                            .foregroundColor(.gray)
-//                            .padding(.horizontal)
-//                        
-//                        Button("Close") {
-//                            mapViewModel.isShowModal = false
-//                        }
-//                        .padding(.bottom, 20)
-//                        
-//                        Button {
-//                            withAnimation {
-//                                mapViewModel.movePlace()
-//                            }
-//                        } label: {
-//                            Text("Teleport")
-//                        }
-//                        .frame(width: 350)
-//                        .buttonStyle(OnboardingButtonStyle())
-//                        
-//                    }
-//                    .frame(maxWidth: .infinity, maxHeight: .infinity)
-//                    .background(Color.white)
-//                    .cornerRadius(10)
-//                    .shadow(radius: 5)
-//                    .padding(10)
-//                }
-//                
-//            }
-//            .tabViewStyle(PageTabViewStyle())
-//            .frame(height: 300)
-//        }
-//        .frame(maxWidth: .infinity)
-//        .background(Color.white)
-//        .cornerRadius(20)
-//        .shadow(radius: 10)
-//        .transition(.move(edge: .bottom))
-//        .animation(.easeInOut)
-//    }
-//}
-//
-//
-//
+            TabView {
+                ForEach(Array(viewModel.cards.enumerated()), id: \.element.id) { index, card in
+                    VStack {
+                        Image(card.imageName)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 350, height: 100)
+                        Text(card.imageName)
+                        Text("02-1234-1234")
+                        Text("서울특별시 중구 창경궁로 62-29")
+                    }
+                    .tag(index)
+                }
+            }
+            .tabViewStyle(PageTabViewStyle())
+            .frame(height: 200)
+            .onChange(of: selectedIndex) { newIndex in
+                mapViewModel.movePlace()
+            }
+        }
+        .frame(maxWidth: .infinity)
+        .background(Color.white)
+        .cornerRadius(20)
+        .shadow(radius: 10)
+        .transition(.move(edge: .bottom))
+        .animation(.easeInOut)
+    }
+}
+
+#Preview {
+    SheetTestView()
+}
