d7e98003054b687ac453bf391b54c9cb413b3575
Mango <mangodev4@gmail.com>
2024-09-20 10:33:46 +0900

    feat. 새로운 맵뷰 생성 구현
    기존 뷰에서 필요한 기능만 추출하기 위한 뷰 생성

---
+++ PYNMApp/Views/NMMapView.swift
@@ -0,0 +1,20 @@
+//
+//  NMMapView.swift
+//  PYNMApp
+//
+//  Created by Yujin Son on 9/20/24.
+//
+
+import SwiftUI
+import MapKit
+
+
+struct NMMapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NMMapView()
+}
