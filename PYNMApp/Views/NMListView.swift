ec064372d594c34161ead65539049ac7cbaac5cd
Mango <mangodev4@gmail.com>
2024-09-25 10:04:28 +0900

    feat. 리스트뷰 버튼 제거 및 주석 제거
    버튼 제거 후 기존 주석처리한 코드들 제거

--- PYNMApp/Views/NMListView.swift
+++ PYNMApp/Views/NMListView.swift
@@ -10,83 +10,51 @@ import SwiftUI
 struct NMListView: View {
     @ObservedObject var cardListViewModel = CardListViewModel()
     @StateObject var mapViewModel = MapViewModel()
-//    @StateObject var navigationManager = NavigationManager()
-//    @EnvironmentObject var navigationManager: NavigationManager
-//    @Environment(\.presentationMode) var presentationMode
-
-
+    //    @Environment(\.presentationMode) var presentationMode
     
     var body: some View {
-//        NavigationStack(path: $navigationManager.path) {
-            ZStack(alignment: .top){
-                VStack {
-                    StickyHeader()
-                    ScrollView {
-                        LazyVStack {
-                            ForEach(cardListViewModel.cards) { card in
-                                VStack {
-                                    Image(card.imageName)
-                                        .resizable()
-                                        .scaledToFill()
-                                        .cornerRadius(8)
-                                        .opacity(card.opacity)
-                                    Text(card.imageName)
-                                }
+        ZStack(alignment: .top){
+            VStack {
+                HeaderSection()
+                ScrollView {
+                    LazyVStack {
+                        ForEach(cardListViewModel.cards) { card in
+                            VStack {
+                                Image(card.imageName)
+                                    .resizable()
+                                    .scaledToFill()
+                                    .cornerRadius(8)
+                                    .opacity(card.opacity)
+                                Text(card.imageName)
                             }
-                            .frame(maxWidth: .infinity, maxHeight: 150)
-                            .padding(.vertical, 10)
-                            .padding(.horizontal, 5)
-                            .background(Color.clear)
                         }
+                        .frame(maxWidth: .infinity, maxHeight: 150)
+                        .padding(.vertical, 10)
+                        .padding(.horizontal, 5)
+                        .background(Color.clear)
                     }
-                
-                
+                }
                 HStack(alignment: .top) {
                     Spacer()
-
-                    Button {
-                        mapViewModel.isShowList = false
-                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
-//                            mapViewModel.isShowModal = true
-                        }
-
-                        
-                    } label: {
-                        Text("View Map")
-                    }
-                    .buttonStyle(HeaderButtonStyle())
                 }
                 .padding(.top, 10)
-                .zIndex(0)
             }
-            }
-//        .sheet(isPresented: $mapViewModel.isShowModal) {
-//            NMMapView(navigationManager: NavigationManager())
-//        }
-//            .navigationDestination(for: Card.self) { card in
-//                NMMapView()
-//                    .environmentObject(navigationManager)
-//                    .onAppear {
-//                        if let index = cardListViewModel.cards.firstIndex(where: { $0.id == card.id }) {
-//                            mapViewModel.selectedCardIndex = index
-//                        }
-//                    }
-//            }
         }
     }
+}
 
-// MARK: Sticky Header
-struct StickyHeader: View {
+// MARK: Header Section
+struct HeaderSection: View {
     var body: some View {
         Spacer()
 
         VStack {
             Spacer()
-            Text("평양 냉면")
-                .fontWeight(.bold)
-            Text("서울 맛집 20선")
-            Spacer()
-            Divider()
+//            Text("평양 냉면")
+//                .fontWeight(.bold)
+//            Text("서울 맛집 20선")
+//            Spacer()
+//            Divider()
         }
         .frame(minWidth: 0, maxWidth: .infinity)
         .frame(height: 50)
