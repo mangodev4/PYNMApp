cd7cf766b14135c08b5f4841767a7a8be163108c
Mango <mangodev4@gmail.com>
2024-09-25 15:28:14 +0900

    feat. 맵뷰모델 span 값 통일 구현
    장소 전환시 span값 바뀌지 않도록 통일시킴

--- PYNMApp/ViewModels/MapViewModel.swift
+++ PYNMApp/ViewModels/MapViewModel.swift
@@ -50,7 +50,7 @@ class MapViewModel: ObservableObject {
         withAnimation {
             self.region = MKCoordinateRegion(
                 center: place.coordinate,
-                span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+                span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
             )
         }
     }
@@ -59,7 +59,7 @@ class MapViewModel: ObservableObject {
             withAnimation(Animation.linear(duration: 4.0)) {
                 selectedCardIndex = (selectedCardIndex + 1) % places.count
                 region.center = places[selectedCardIndex].coordinate
-                region.span = MKCoordinateSpan(latitudeDelta: 0.005, longitudeDelta: 0.005)
+                region.span = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
             }
         }
     
